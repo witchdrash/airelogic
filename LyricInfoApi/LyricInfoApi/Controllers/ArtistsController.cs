5676104518fc597f1b1d58c15148809e53b63845
Owen Ashcroft <owen.ashcroft@autumn-wren.co.uk>
2020-07-18 12:52:25 +0100

    Quick stub to check I can hit the API successfully. Removed WeatherForecastController

---
+++ LyricInfoApi/LyricInfoApi/Controllers/ArtistsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LyricInfoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArtistsController : ControllerBase
+    {
+        [HttpPost("search")]
+        public IEnumerable<Artist> Search(string artistName)
+        {
+            return new []{ new Artist(artistName), };
+        }
+    }
+
+    public class Artist
+    {
+        public Artist(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; }
+    }
+}
